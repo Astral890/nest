4e18323b165113591e39bcd5b6ba1b55f6b3c91c
Astral890 <enac7125@outlook.com>
2024-10-18 10:52:49 -0600

    Cambios en la escuela

--- src/app/pokeapi/page.js
+++ src/app/pokeapi/page.js
@@ -1,5 +1,28 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 export default function pokeapi(){
-
+    const [poke, setPoke]=useState([]);
+    const [namePoke, setName]=useState();
+    return(
+        <div>
+            <input type="text" placeholder="Pokemon a buscar" onChange={()=>handleInputChange(event)}></input>
+            <p>{namePoke}</p>
+            <button onClick={()=>getPoke()}>Buscar Pokemon</button>
+            <ul>
+                <li>
+                    {poke.name}
+                </li>
+            </ul>
+        </div>
+    )
+    function handleInputChange(e){
+        setName(e.target.value);
+    }
+    async function getPoke(){
+        await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+        .then(response=>response.json())
+        .then((data)=>{
+            setPoke(data);
+        });
+    }
 }
